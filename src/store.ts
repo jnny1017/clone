23b2fd930da96e9391a468e2aa01cb6fd7050f12
jnny1017 <jnny1017@gmail.com>
2022-03-12 16:19:24 +0900

    combineReducers를 사용하여 수정

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import mainReducer from 'mainSlice'
 
+const rootReducers = {
+  main: mainReducer,
+}
+
+export const reducers = combineReducers({
+  ...rootReducers,
+});
+
+export type RootState = ReturnType<typeof reducers>
+
 export const store = configureStore({
-  reducer: {
-    main: mainReducer,
-  },
+  reducer: rootReducers,
 })
 
-export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export default store
