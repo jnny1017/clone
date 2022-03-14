e8099780fa9073687bb6f2d1d4152bf92c764d14
jnny1017 <jnny1017@gmail.com>
2022-03-14 10:17:43 +0900

    SectionProductModel 타입 분리

--- src/components/main/section/models/section.model.ts
+++ src/components/main/section/models/section.model.ts
@@ -2,14 +2,16 @@ export interface SectionModel {
   title: string,
   subtitle: string,
   data: {
-    products: {
-      no: number,
-      name: string,
-      short_description: string,
-      list_image_url: string,
-      original_price: number,
-      discounted_price: number,
-      discount_rate: number,
-    }[]
+    products: SectionProductModel[]
   }
+}
+
+export interface SectionProductModel {
+  no: number,
+  name: string,
+  short_description: string,
+  list_image_url: string,
+  original_price: number,
+  discounted_price: number,
+  discount_rate: number,
 }
