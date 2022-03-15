3312a3ee449c9cdedb16d4ac905131bc5297560c
jnny1017 <jnny1017@gmail.com>
2022-03-15 14:46:07 +0900

    LazyComponent 생성

--- src/components/lazy/LazyComponent.tsx
+++ src/components/lazy/LazyComponent.tsx
@@ -1,19 +1,18 @@
+import { PropsWithChildren } from 'react';
 import LazyLoad from 'react-lazyload';
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default function Lazy({ children }: Props) {
+export default function LazyComponent({ children }: PropsWithChildren<Props>) {
   return (
     <LazyLoad
       once={true}
       placeholder={<div>loading</div>}
       height={200}
-      offset={[-100, 0]}
     >
       {children}
     </LazyLoad>
   );
 };
-
--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,24 +1,21 @@
 import { MainBanner } from 'components/main/mainBanner';
 import { MdChoiceSection } from 'components/main/mdChoice';
 import { CollectionSectionContainer } from 'components/main/randomCollection';
-import Lazy from 'components/lazy/LazyComponent'
+import LazyComponent from 'components/lazy'
 import * as S from 'styles/mainStyles';
-import LazyImage from 'components/lazy/LazyImg'
 
 export default function MainPage() {
   return (
     <>
       <MainBanner />
       <S.Main>
-        <Lazy>
+        <LazyComponent>
           <MdChoiceSection />
-        </Lazy>
-        {[...Array(50).keys()].map(i => (
-          <LazyImage
-            key={i}
-            src={`https://picsum.photos/1000/1000?random=${i}`}
-            alt={`Random image ${i}`}
-          />
+        </LazyComponent>
+        {[...Array(5).keys()].map(i => (
+          <LazyComponent key={i}>
+            <CollectionSectionContainer />
+          </LazyComponent>
         ))}
       </S.Main>
     </>
