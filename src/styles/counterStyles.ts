257434b6f9bc95f0358957069dd835e5645fd7f2
jnny1017 <jnny1017@gmail.com>
2022-03-26 18:20:22 +0900

    chore: counter스타일 수정

--- src/styles/counterStyles.ts
+++ src/styles/counterStyles.ts
@@ -3,13 +3,11 @@ import { COLORS } from './colorStyles';
 
 export const Counter = styled.div`
   display: flex;
-  width: 88px;
-  height: 30px;
   border: 1px solid #dddfe1;
   border-radius: 3px;
 `;
 
-export const Button = styled.button`
+const Button = styled.button`
   width: 28px;
   height: 28px;
 `;
@@ -17,12 +15,10 @@ export const Button = styled.button`
 export const Input = styled.input`
   width: 30px;
   height: 30px;
-  margin-right: -1px;
-  padding: 0 0 4px;
+  padding: 0;
   border: 0;
   background-color: ${COLORS.white};
-  font-size: 14px;
   color: ${COLORS.black};
-  line-height: 18px;
+  font-size: 14px;
   text-align: center;
 `;
